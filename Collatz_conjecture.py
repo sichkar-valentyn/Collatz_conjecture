65e0d66489108ae2313f2354462892f1f73b38f3
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-07-09 21:53:05 +0300

    Update Collatz_conjecture.py

--- Collatz_conjecture.py
+++ Collatz_conjecture.py
@@ -1,3 +1,16 @@
+# File: Collatz_conjecture.py
+# Description: Implementing Collatz conjecture by recursive function
+# Environment: PyCharm and Anaconda environment
+#
+# MIT License
+# Copyright (c) 2018 Valentyn N Sichkar
+# github.com/sichkar-valentyn
+#
+# Reference to:
+# [1] Valentyn N Sichkar. Implementing Collatz conjecture by recursive function // GitHub platform [Electronic resource]. URL: https://github.com/sichkar-valentyn/Collatz_conjecture (date of access: XX.XX.XXXX)
+
+
+
 # Collatz conjecture
 # Creating a recursive function for calculating sequence of numbers,
 # described by Collatz conjecture
